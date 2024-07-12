775ff19f77a37bf6f1a72c3ffb0733dd63f15e54
Dylan010 <moscadylan@gmail.com>
2024-07-12 19:36:50 -0300

    vagrantfile y terraform ya listos

--- Vagrantfile
+++ Vagrantfile
@@ -1,12 +1,35 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+TERRAFORM_VERSION = "1.6.6"
+
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/focal64"
+
   config.vm.network "private_network", ip: "192.168.33.10"
+
   config.vm.provider "virtualbox" do |vb|
-    vb.memory = "1024"
+    vb.memory = "2048"
     vb.cpus = 2
   end
+
   config.vm.provision "shell", inline: <<-SHELL
-    apt-get update
-    apt-get install -y apache2
+    # Actualizar repositorios e instalar dependencias
+    sudo apt-get update
+    sudo apt-get install -y unzip wget software-properties-common
+
+    # Instalar Terraform
+    wget https://releases.hashicorp.com/terraform/#{TERRAFORM_VERSION}/terraform_#{TERRAFORM_VERSION}_linux_amd64.zip
+    unzip terraform_#{TERRAFORM_VERSION}_linux_amd64.zip
+    sudo mv terraform /usr/local/bin/
+    rm terraform_#{TERRAFORM_VERSION}_linux_amd64.zip
+
+    # Instalar Ansible
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt-get install -y ansible
+
+    # Verificar las instalaciones
+    terraform --version
+    ansible --version
   SHELL
 end
---
+++ main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+
+resource "aws_instance" "Desafio_11" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = "bootcamp"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "test-server"
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.Desafio_11.public_ip
+}
